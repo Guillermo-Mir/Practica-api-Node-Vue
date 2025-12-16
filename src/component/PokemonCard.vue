9ad842acca50b55edf3b2837dff13bc4d52212a1
Guillermo-Mir <guillermo.mir.7e8@itb.cat>
2025-12-16 19:02:20 +0100

    Estructura de carpetas

---
+++ src/component/PokemonCard.vue
@@ -0,0 +1,16 @@
+<script setup>
+const props = defineProps({
+    p: {
+        type: Object,
+        required:true
+    }
+})
+</script>
+
+<template>
+<div>
+    <h3>{{ props.p.name }}</h3>
+    <p>{{ props.p.type }} - {{ props.p.generation }} </p>
+    <!--RouterLink :to="{name:'MealDetail', params:{idMeal:p.idMeal}}">Recepta</RouterLink-->
+</div>
+</template>
